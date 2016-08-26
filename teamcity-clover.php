2dac878bf37ccb4c3cf863bbe00dd01cb87c9e0e
Lewis Wright <LewisW@users.noreply.github.com>
2016-08-26 11:01:04 +0100

    Add support for multiple files

--- teamcity-clover.php
+++ teamcity-clover.php
@@ -16,71 +16,77 @@ if ($argc < 2) {
 
 $options = getopt('', array('crap-threshold:'));
 $crapThreshold = array_key_exists('crap-threshold', $options) ? (float) $options['crap-threshold'] : 30;
-
-$path = array_pop($argv);
-if (!file_exists($path)) {
-    echo "clover.xml does not exist: $path\n";
-    exit(1);
-}
-
-
-echo "Parsing clover.xml from: $path\n";
-$cloverXml = new SimpleXMLElement($path, null, true);
-$metrics = $cloverXml->project->metrics;
-
-if (!$metrics) {
-    echo "clover.xml does not contain code coverage metrics.\n";
-    exit(1);
-}
-
-$coveredClasses = 0;
-foreach ($cloverXml->xpath('//class') as $class) {
-    if ((int) $class->metrics['coveredmethods'] === (int) $class->metrics['methods']) {
-        $coveredClasses++;
-    }
-}
-
-
 $data = array(
-    'CodeCoverageAbsLTotal' => (int) $metrics['elements'],
-    'CodeCoverageAbsLCovered' => (int) $metrics['coveredelements'],
-    'CodeCoverageAbsBTotal' => (int) $metrics['statements'],
-    'CodeCoverageAbsBCovered' => (int) $metrics['coveredstatements'],
-    'CodeCoverageAbsMTotal' => (int) $metrics['methods'],
-    'CodeCoverageAbsMCovered' => (int) $metrics['coveredmethods'],
-    'CodeCoverageAbsCTotal' => (int) $metrics['classes'],
-    'CodeCoverageAbsCCovered' => $coveredClasses,
-    'CodeCoverageB' => $metrics['statements'] ? $metrics['coveredstatements'] / $metrics['statements'] * 100 : 0,
-    'CodeCoverageL' => $metrics['elements'] ? $metrics['coveredelements'] / $metrics['elements'] * 100 : 0,
-    'CodeCoverageM' => $metrics['methods'] ? $metrics['coveredmethods'] / $metrics['methods'] * 100 : 0,
-    'CodeCoverageC' => $metrics['classes'] ? $coveredClasses / $metrics['classes'] * 100 : 0,
-    'Files' => (int) $metrics['files'],
-    'LinesOfCode' => (int) $metrics['loc'],
-    'NonCommentLinesOfCode' => (int) $metrics['ncloc'],
+    'CodeCoverageAbsLTotal' => 0,
+    'CodeCoverageAbsLCovered' => 0,
+    'CodeCoverageAbsBTotal' => 0,
+    'CodeCoverageAbsBCovered' => 0,
+    'CodeCoverageAbsMTotal' => 0,
+    'CodeCoverageAbsMCovered' => 0,
+    'CodeCoverageAbsCTotal' => 0,
+    'CodeCoverageAbsCCovered' => 0,
+    'Files' => 0,
+    'LinesOfCode' => 0,
+    'NonCommentLinesOfCode' => 0
 );
 
+foreach (array_slice($argv, 1) as $path) {
+    if (!file_exists($path)) {
+        echo "clover.xml does not exist: $path\n";
+        exit(1);
+    }
 
-if ($crapThreshold) {
-    $crapValues = array();
-    $crapAmount = 0;
-    foreach ($cloverXml->xpath('//@crap') as $crap) {
-        $crap = (float) $crap;
-        $crapValues[] = $crap;
-        if ($crap >= $crapThreshold) {
-            $crapAmount++;
+
+    echo "Parsing clover.xml from: $path\n";
+    $cloverXml = new SimpleXMLElement($path, null, true);
+    $metrics = $cloverXml->project->metrics;
+
+    if (!$metrics) {
+        echo "clover.xml does not contain code coverage metrics.\n";
+        exit(1);
+    }
+
+    $coveredClasses = 0;
+    foreach ($cloverXml->xpath('//class') as $class) {
+        if ((int) $class->metrics['coveredmethods'] === (int) $class->metrics['methods']) {
+            $coveredClasses++;
         }
     }
 
-    $crapValuesCount = count($crapValues);
-    $crapTotal = array_sum($crapValues);
+    $data['CodeCoverageAbsLTotal'] += (int) $metrics['elements'];
+    $data['CodeCoverageAbsLCovered'] += (int) $metrics['coveredelements'];
+    $data['CodeCoverageAbsBTotal'] += (int) $metrics['statements'];
+    $data['CodeCoverageAbsBCovered'] += (int) $metrics['coveredstatements'];
+    $data['CodeCoverageAbsMTotal'] += (int) $metrics['methods'];
+    $data['CodeCoverageAbsMCovered'] += (int) $metrics['coveredmethods'];
+    $data['CodeCoverageAbsCTotal'] += (int) $metrics['classes'];
+    $data['CodeCoverageAbsCCovered'] += $coveredClasses;
+    $data['Files'] += (int) $metrics['files'];
+    $data['LinesOfCode'] += (int) $metrics['loc'];
+    $data['NonCommentLinesOfCode'] += (int) $metrics['ncloc'];
 
-    $data['CRAPAmount'] = $crapAmount;
-    $data['CRAPPercent'] = $crapValuesCount ? $crapAmount / $crapValuesCount * 100 : 0;
-    $data['CRAPTotal'] = $crapTotal;
-    $data['CRAPAverage'] = $crapValuesCount ? $crapTotal / $crapValuesCount : 0;
-    $data['CRAPMaximum'] = max($crapValues);
+
+    if ($crapThreshold) {
+        $crapValues = array();
+        $crapAmount = 0;
+        foreach ($cloverXml->xpath('//@crap') as $crap) {
+            $crap = (float) $crap;
+            $crapValues[] = $crap;
+            if ($crap >= $crapThreshold) {
+                $crapAmount++;
+            }
+        }
+
+        $crapValuesCount = count($crapValues);
+        $crapTotal = array_sum($crapValues);
+
+        $data['CRAPAmount'] = $crapAmount;
+        $data['CRAPPercent'] = $crapValuesCount ? $crapAmount / $crapValuesCount * 100 : 0;
+        $data['CRAPTotal'] = $crapTotal;
+        $data['CRAPAverage'] = $crapValuesCount ? $crapTotal / $crapValuesCount : 0;
+        $data['CRAPMaximum'] = max($crapValues);
+    }
 }
-
 
 foreach ($data as $key => $value) {
     if (is_float($value)) {
